1b16c7dfda2e04d1ed59185005ff7d7c2f0a9c66
JosiasAurel <ndjosiasaurel@gmail.com>
2021-06-07 16:32:40 +0100

    some elments for the header nav

--- src/index.js
+++ src/index.js
@@ -14,5 +14,26 @@ function initializeApp() {
 initializeApp();
 // create the website header
 var header = document.createElement("header");
-header.innerText = "Hello world";
+// children of header element
+// website title
+var headerH2 = document.createElement("h2");
+headerH2.innerText = "TypeScript Website";
+// create nav links of the site
+var nav = document.createElement("nav");
+// nav children
+var homeAnchor = document.createElement("a");
+homeAnchor.innerText = "Home";
+homeAnchor.style.textDecoration = "underline";
+homeAnchor.style.color = "white";
+var aboutAnchor = document.createElement("a");
+aboutAnchor.innerText = "About";
+aboutAnchor.style.textDecoration = "underline";
+aboutAnchor.style.color = "white";
+// add nav children
+nav.appendChild(homeAnchor);
+nav.appendChild(aboutAnchor);
+// add children of header element
+header.appendChild(headerH2);
+header.appendChild(nav);
+// adding the created elements as children of body or so
 body.appendChild(header);
--- src/index.ts
+++ src/index.ts
@@ -24,6 +24,35 @@ initializeApp();
 // create the website header
 
 const header: HTMLElement = document.createElement("header");
-header.innerText = "Hello world";
 
-body.appendChild(header)
+// children of header element
+
+// website title
+let headerH2: HTMLHeadingElement = document.createElement("h2");
+headerH2.innerText = "TypeScript Website";
+
+// create nav links of the site
+let nav: HTMLElement = document.createElement("nav");
+
+// nav children
+let homeAnchor: HTMLAnchorElement = document.createElement("a");
+homeAnchor.innerText = "Home";
+homeAnchor.style.textDecoration = "underline";
+homeAnchor.style.color = "white";
+let aboutAnchor: HTMLAnchorElement = document.createElement("a");
+aboutAnchor.innerText = "About";
+aboutAnchor.style.textDecoration = "underline";
+aboutAnchor.style.color = "white";
+
+// add nav children
+nav.appendChild(homeAnchor);
+nav.appendChild(aboutAnchor);
+
+// add children of header element
+header.appendChild(headerH2);
+header.appendChild(nav);
+
+
+// adding the created elements as children of body or so
+body.appendChild(header);
+
