80237f8f12e9b7479c9db717b6981b995701b3bc
JosiasAurel <ndjosiasaurel@gmail.com>
2021-06-08 05:55:56 +0100

    remove compiled JS code

--- src/utils.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-exports.__esModule = true;
-exports.createAndSetStyles = void 0;
-function createAndSetStyles(elment, stylesObject) {
-    if (elment === void 0) { elment = document.createElement("p"); }
-    var styleAndProp = stylesObject[0].split(", ").map(function (item) { return item.split(" "); });
-}
-exports.createAndSetStyles = createAndSetStyles;
